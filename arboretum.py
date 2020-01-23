813311fe75461d94727bef73e2e2d979307cbf5d
Ken Boyd <klb417@gmail.com>
2020-01-23 13:36:43 -0600

    completed aroboretum class

--- arboretum.py
+++ arboretum.py
@@ -1,6 +1,47 @@
 class Arboretum:
     def __init__(self, name, address):
-        self.name = name
-        self.address = address
-        self.rivers = []
-        self.grasslands = []
+        self.__name = name
+        self.__address = address
+        self.__biomes = {
+            "rivers": [],
+            "coastlines": [],
+            "forests": [],
+            "grasslands": [],
+            "mountains": [],
+            "swamps": [],
+        }
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def address(self):
+        return self.__address
+
+    @property
+    def rivers(self):
+        return self.__biomes["rivers"]
+
+    @property
+    def coastlines(self):
+        return self.__biomes["coastlines"]
+
+    @property
+    def forests(self):
+        return self.__biomes["forests"]
+
+    @property
+    def grasslands(self):
+        return self.__biomes["grasslands"]
+
+    @property
+    def mountains(self):
+        return self.__biomes["mountains"]
+
+    @property
+    def swamps(self):
+        return self.__biomes["swamps"]
+
+    def annex_biome(self, biome_list_type, biome_to_add):
+        self.__biomes[biome_list_type].append(biome_to_add)
