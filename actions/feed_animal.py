1bdcc37b18b59a9ab4cc03941659f238534e9675
James Chapman <chapmanjamesg@gmail.com>
2020-01-23 14:17:36 -0600

    about to test others code

--- actions/feed_animal.py
+++ actions/feed_animal.py
@@ -1,6 +1,15 @@
 import os
+from river_dolphin import RiverDolphin
 
-def feed_animal(arboretum):
+def feeding_animals(animal):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for index, prey in enumerate(animal.__prey, 1):
+        print(f"{index}. {prey}")
+    choice = input(f"What is on the menu for the {animal.name} today? >")
+    if choice == f"{index}":
+        animal.feed(prey)
+
+def feed_animal():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("1. Gold Dust Day Gecko")
     print("2. River Dolphin")
@@ -9,5 +18,34 @@ def feed_animal(arboretum):
     print("5. Pueo")
     print("6. 'Ulae")
     print("7. Ope'ape'a")
-    print("Happy-Face Spider")
+    print("8. Happy-Face Spider")
 
+    choice = input("Choose animal to feed. >")
+
+    if choice == "1":
+        g_d_d_gecko = Gold_Dust_Day_Gecko()
+        pass
+    if choice == "2":
+        river_dolphin = RiverDolphin()
+        feeding_animals(river_dolphin)
+    if choice == "3":
+        nene_goose = Nene_Goose()
+        pass
+    if choice == "4":
+        kikakapu = Kikakapu()
+        pass
+    if choice == "5":
+        pueo = Pueo()
+        pass
+    if choice == "6":
+        ulae = Ulae()
+        pass
+    if choice == "7":
+        opeapea = Opeapea()
+        pass
+    if choice == "8":
+        hhfspider = Happy_Face_Spider()
+        pass
+
+
+feed_animal()
