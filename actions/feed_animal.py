58e031e8a8b5657bedaa9d493996a4abc3bd9a14
James Chapman <chapmanjamesg@gmail.com>
2020-01-23 11:23:18 -0600

    trying ken's pr

---
+++ actions/feed_animal.py
@@ -0,0 +1,13 @@
+import os
+
+def feed_animal(arboretum):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("1. Gold Dust Day Gecko")
+    print("2. River Dolphin")
+    print("3. Nene Goose")
+    print("4. Kikakapu")
+    print("5. Pueo")
+    print("6. 'Ulae")
+    print("7. Ope'ape'a")
+    print("Happy-Face Spider")
+
