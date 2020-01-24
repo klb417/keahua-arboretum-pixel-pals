2d35381e6af5f1af37f0d185801da8ddb22875db
Ryan Crowley <ryancrowleycode@gmail.com>
2020-01-24 09:49:46 -0600

    pressing 4 on the main menu now brings the user to the plants menu. application is then waiting for input from the user.

--- actions/cultivate_plant.py
+++ actions/cultivate_plant.py
@@ -1,3 +1,22 @@
+import os
+
+
+# Function for adding a plant to the arboretum
 def cultivate_plant(arboretum):
 
-    input("please finish me")
+    # Clears the terminal of previous contents
+    os.system("cls" if os.name == "nt" else "clear")
+
+    # Printing menu options to the console
+    print('''Plants
+    ''')
+    print("1. Rainbow Eucalyptus Tree")
+    print("2. Silversword")
+    print("3. Mountain Apple Tree")
+    print("4. Blue Jade Vine")
+
+    # Capturing user input and assigning it to "choice" variable
+    choice = input('''
+Choose a plant to cultivate, or
+Type M to return to the main menu. > ''')
+
