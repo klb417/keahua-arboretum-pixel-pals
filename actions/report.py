4338092b270170adde243ee78bee3088a428c030
Ken Boyd <klb417@gmail.com>
2020-01-24 08:59:37 -0600

    report prints annexed biomes only, not inhabitants

--- actions/report.py
+++ actions/report.py
@@ -1,5 +1,9 @@
 def build_facility_report(arboretum):
-    for river in arboretum.rivers:
-        print(f'River [{river.id}]')
+    biomes = ["coastlines", "forests", "grasslands", "mountains", "rivers", "swamps"]
+    for biome_category in biomes:
+        print(biome_category.capitalize())
+        for biome in arboretum.biomes[biome_category]:
+            print(f"  {biome.name} [{str(biome.id)[:8]}]")
+        print("")
 
     input("\n\nPress any key to continue...")
