7e55a55d23ca420911734ac95c772f742b4d564d
Ryan Crowley <ryancrowleycode@gmail.com>
2020-01-24 13:27:57 -0600

    user can now add plants to different biomes

--- actions/show_plant_biomes.py
+++ actions/show_plant_biomes.py
@@ -18,12 +18,21 @@ def show_plant_biomes(plant, biome_list):
         for index, biome in enumerate(biome_list):
             print(f'{index + 1}. {biome.name} ( plants)')
         
-        choice = input(f'''
-Where would you like to cultivate the {plant.species}?
+        choice = input(f'''\nWhere would you like to cultivate the {plant.species}?
 Type M to return to the main menu. > ''')
+
+        if choice.lower() == "m":
+            return
+        else:
+            try: 
+                selected_biome = biome_list[int(choice) -1]
+                selected_biome.add_plant(plant)
+                print(f"\nThe {plant.species} was added to {selected_biome.name}.")
+                input('\n\n Type any key to return to the main menu.')
+            except:
+                show_plant_biomes(plant, biome_list)
+
     else:
-        input(f'''Whoops!
-
+        input(f'''Whoops!\n
 There are no available habitats in the arboretum that are suitable for {plant.species}.
-
-Press any key to return to the main menu. ''')
+\nPress any key to return to the main menu. ''')
