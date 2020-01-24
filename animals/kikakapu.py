5a51251c4183c03bd2abe9316aeca617f24e7dab
James Chapman <chapmanjamesg@gmail.com>
2020-01-24 11:07:37 -0600

    made it so that feeding animals works

--- animals/kikakapu.py
+++ animals/kikakapu.py
@@ -1,10 +1,10 @@
 from animals import Animal
 from interfaces import IFreshwater
 from interfaces import Identifiable
-class Kīkākapu(Animal, IFreshwater, Identifiable):
+class Kikakapu(Animal, IFreshwater, Identifiable):
 
     def __init__(self):
-        Animal.__init__(self, "Kīkākapu")
+        Animal.__init__(self, "Kikakapu")
         IFreshwater.__init__(self)
         Identifiable.__init__(self)
         self.__prey = { "Planktonic Animals", "Vegetation", "Trout", "Mackarel", "Salmon", "Sardine" }
@@ -15,10 +15,10 @@ class Kīkākapu(Animal, IFreshwater, Identifiable):
 
     def feed(self, prey):
         if prey in self.__prey:
-            print(f"The Kīkākapu ate {prey} for a meal")
+            print(f"The Kikakapu ate {prey} for a meal")
         else:
-            print(f"The Kīkākapu rejects the {prey}")
+            print(f"The Kikakapu rejects the {prey}")
 
 
     def __str__(self):
-        return f"Kīkākapu {self.id}. I want more Planktonic Animals but...... Crunchberries will do!"
+        return f"Kikakapu {self.id}. I want more Planktonic Animals but...... Crunchberries will do!"
--- index.py
+++ index.py
@@ -18,49 +18,49 @@ from plants import (
     BlueJadeVine
 )
 
-from animals import (RiverDolphin, nenegoose, Ulae, Kīkākapu, Pueo, Opeapea, HHFSpider, GDDGecko)
+from animals import (RiverDolphin, nenegoose, Ulae, Kikakapu, Pueo, Opeapea, HHFSpider, GDDGecko)
+
+feed_animal()
+
+# keahua = Arboretum("Keahua Arboretum", "123 Paukauila Lane")
+
+# def build_menu():
+#     os.system("cls" if os.name == "nt" else "clear")
+#     print("1. Annex Habitat")
+#     print("2. Release Animal into Habitat")
+#     print("3. Feed Animal")
+#     print("4. Add Plant to Habitat")
+#     print("5. Display Facility Report")
+#     print("6. Exit")
 
 
+# def main_menu():
+#     """Show Keahua Action Options
 
-keahua = Arboretum("Keahua Arboretum", "123 Paukauila Lane")
+#     Arguments: None
+#     """
+#     build_menu()
+#     choice = input(">> ")
 
-def build_menu():
-    os.system("cls" if os.name == "nt" else "clear")
-    print("1. Annex Habitat")
-    print("2. Release Animal into Habitat")
-    print("3. Feed Animal")
-    print("4. Add Plant to Habitat")
-    print("5. Display Facility Report")
-    print("6. Exit")
-
-
-def main_menu():
-    """Show Keahua Action Options
-
-    Arguments: None
-    """
-    build_menu()
-    choice = input(">> ")
-
-    if choice == "1":
-        annex_biome(keahua)
+#     if choice == "1":
+#         annex_biome(keahua)
 
 
-    if choice == "2":
-        release_animal(keahua)
+#     if choice == "2":
+#         release_animal(keahua)
 
-    if choice == "3":
-        feed_animal(keahua)
+#     if choice == "3":
+#         feed_animal(keahua)
 
-    if choice == "4":
-        cultivate_plant(keahua)
+#     if choice == "4":
+#         cultivate_plant(keahua)
 
-    if choice == "5":
-        build_facility_report(keahua)
-        pass
+#     if choice == "5":
+#         build_facility_report(keahua)
+#         pass
 
-    if choice != "6":
-        main_menu()
+#     if choice != "6":
+#         main_menu()
 
 
-main_menu()
+# main_menu()
