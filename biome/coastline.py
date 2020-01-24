a6c6445e7ce5001b933c2b94dcf700ba7a966b2d
Ken Boyd <klb417@gmail.com>
2020-01-24 13:46:58 -0600

    starter code for limiting plants and animals

--- biome/coastline.py
+++ biome/coastline.py
@@ -5,17 +5,15 @@ from .biome import Biome
 
 
 class Coastline(IContainsAnimals, IContainsPlants, Identifiable, Biome):
-
     def __init__(self, name):
-        IContainsAnimals.__init__(self)
-        IContainsPlants.__init__(self)
+        IContainsAnimals.__init__(self, 15)
+        IContainsPlants.__init__(self, 3)
         Identifiable.__init__(self)
         Biome.__init__(self, name)
 
     def add_animal(self, animal):
         # Add in checks for animal type
         self.animals.append(animal)
-
 
     def add_plant(self, plant):
         # Add in checks for plant type
--- interfaces/habitat/contains_plants.py
+++ interfaces/habitat/contains_plants.py
@@ -1,4 +1,11 @@
-class IContainsPlants():
+class IContainsPlants:
+    def __init__(self, maximum_plants=0):
+        self.__plants = []
+        self.__maximum_plants = maximum_plants
 
-    def __init__(self):
-        self.plants = []
+    @property
+    def plants(self):
+        return self.__plants
+
+    def is_plants_not_full():
+        return len(self.plants) < self.__maximum_plants
