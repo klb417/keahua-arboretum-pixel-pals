c83f37f3729ebf09ba71827382628ab85c6e3da5
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-23 20:34:05 -0600

    Added comments

--- actions/annex.py
+++ actions/annex.py
@@ -4,8 +4,11 @@ from biome import River, Forest
 
 # Function for adding a biome to the arboretum
 def annex_biome(arboretum):
+
     # Clears the terminal of previous contents
     os.system("cls" if os.name == "nt" else "clear")
+
+    # Printing menu options to the console
     print("1. Mountain")
     print("2. Swamp")
     print("3. Grassland")
@@ -13,8 +16,10 @@ def annex_biome(arboretum):
     print("5. River")
     print("6. Coastline")
 
+    # Capturing user input and assigning it to "choice" variable
     choice = input("Choose biome to annex > ")
 
+    # if statements check user input, when it evaluates to true the selected biome is appended in arboretum dictionary
     if choice == "1":
         # mountain = Mountain()
         # arboretum.mountains.append(mountain)
--- biome/forest.py
+++ biome/forest.py
@@ -13,15 +13,11 @@ class Forest(IContainsAnimals, IContainsPlants, Identifiable, Biome):
         Biome.__init__(self, name)
 
     def add_animal(self, animal):
-        try:
-            if animal.aquatic and animal.cell_type == "hypertonic":
-                self.animals.append(animal)
-        except AttributeError:
-            raise AttributeError("Cannot add non-aquatic, or saltwater animals to a river")
+        # Add in checks for animal type
+        self.animals.append(animal)
+
 
     def add_plant(self, plant):
-        try:
-            if plant.freshwater and plant.requires_current:
-                self.plants.append(plant)
-        except AttributeError:
-            raise AttributeError("Cannot add plants that require brackish water or stagnant water to a river biome")
+        # Add in checks for plant type
+        self.plants.append(plant)
+   
