0e168eb64960b205a48118bb0a5499e992635efe
Ryan Crowley <ryancrowleycode@gmail.com>
2020-01-24 11:45:31 -0600

    show_plant_biomes clears screen, prints header, only shows options that apply to plant.

--- actions/cultivate_plant.py
+++ actions/cultivate_plant.py
@@ -4,9 +4,15 @@ from .get_plant_biomes import get_plant_biomes
 
 # Function for displaying the possible biomes a plant can be added to
 def show_plant_biomes(plant, biome_list):
+    # Clears the terminal of previous contents
+    os.system("cls" if os.name == "nt" else "clear")
+    print('Cultivate Plant \n \n')
     for index, biome in enumerate(biome_list):
         print(f'{index + 1}. {biome.name} ( plants)')
-    choice = input()
+    
+    choice = input(f'''
+Where would you like to cultivate the {plant.species}?
+Type M to return to the main menu. > ''')
 
 
 # Function for adding a plant to the arboretum
