d3a8a7b682d7e6e1c62cb98d841467c0ab9b9b2c
James Chapman <chapmanjamesg@gmail.com>
2020-01-24 11:10:30 -0600

    took the argument out of feed animals

--- index.py
+++ index.py
@@ -20,47 +20,46 @@ from plants import (
 
 from animals import (RiverDolphin, nenegoose, Ulae, Kikakapu, Pueo, Opeapea, HHFSpider, GDDGecko)
 
-feed_animal()
 
-# keahua = Arboretum("Keahua Arboretum", "123 Paukauila Lane")
+keahua = Arboretum("Keahua Arboretum", "123 Paukauila Lane")
 
-# def build_menu():
-#     os.system("cls" if os.name == "nt" else "clear")
-#     print("1. Annex Habitat")
-#     print("2. Release Animal into Habitat")
-#     print("3. Feed Animal")
-#     print("4. Add Plant to Habitat")
-#     print("5. Display Facility Report")
-#     print("6. Exit")
-
-
-# def main_menu():
-#     """Show Keahua Action Options
-
-#     Arguments: None
-#     """
-#     build_menu()
-#     choice = input(">> ")
-
-#     if choice == "1":
-#         annex_biome(keahua)
+def build_menu():
+    os.system("cls" if os.name == "nt" else "clear")
+    print("1. Annex Habitat")
+    print("2. Release Animal into Habitat")
+    print("3. Feed Animal")
+    print("4. Add Plant to Habitat")
+    print("5. Display Facility Report")
+    print("6. Exit")
 
 
-#     if choice == "2":
-#         release_animal(keahua)
+def main_menu():
+    """Show Keahua Action Options
 
-#     if choice == "3":
-#         feed_animal(keahua)
+    Arguments: None
+    """
+    build_menu()
+    choice = input(">> ")
 
-#     if choice == "4":
-#         cultivate_plant(keahua)
-
-#     if choice == "5":
-#         build_facility_report(keahua)
-#         pass
-
-#     if choice != "6":
-#         main_menu()
+    if choice == "1":
+        annex_biome(keahua)
 
 
-# main_menu()
+    if choice == "2":
+        release_animal(keahua)
+
+    if choice == "3":
+        feed_animal()
+
+    if choice == "4":
+        cultivate_plant(keahua)
+
+    if choice == "5":
+        build_facility_report(keahua)
+        pass
+
+    if choice != "6":
+        main_menu()
+
+
+main_menu()
