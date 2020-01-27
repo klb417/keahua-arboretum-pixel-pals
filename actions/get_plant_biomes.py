2e4d2f04d5bd3a807b2f7e159cfd036ec7649c8c
Ryan Crowley <ryancrowleycode@gmail.com>
2020-01-27 11:08:31 -0600

    commenting code

--- actions/get_plant_biomes.py
+++ actions/get_plant_biomes.py
@@ -13,12 +13,15 @@ def get_plant_biomes(plant, arboretum):
     all_biomes = arboretum.biomes
     for required_biome in required_biomes:
         for potential_biome, value in all_biomes.items():
-            # if the biome in the arboretum is in required biomes of given plant
+            # Checking to see if the category of biome in the arboretum
+            # is an acceptable category for what the plant requires
             if potential_biome == required_biome:
                 for biome in all_biomes[potential_biome]:
-                    # check each biome to make sure it is not full
+                    # Check each biome to make sure it is not full. This
+                    # prevents users from over-populating a biome with plants.
                     if not biome.is_plants_full():
                         # add biomes to plant_biomes list
                         plant_biomes.append(biome)
-            
+                        
+    # Return list of acceptable biomes.
     return plant_biomes
