8b20ecacc9ebc671b5281827cbf0684c607326a1
Lauren Riddle <lari7132@gmail.com>
2020-01-28 10:04:02 -0600

    added module to option 3

--- actions/create_animal.py
+++ actions/create_animal.py
@@ -16,10 +16,10 @@ def create_animal_two_habitats(biome1, biome2, arb, animal):
 
     else: 
 
-        # combine the rivers and coastline lists
+        # combine the biome lists
         new_list = []
         for habitat1 in biome1:
-            # add the river to the list if it has less than 12 animals in it
+            # add the habitat to the list if it has less than 12 animals in it
             if habitat1.is_animals_full() == False:
                 new_list.append(habitat1)
         for habitat2 in biome2:
@@ -43,15 +43,15 @@ def create_animal_two_habitats(biome1, biome2, arb, animal):
                 return
             try:
                 selection = new_list[int(choice1) - 1].id
-                # search the river list for the selection
+                # search the list1 for the selection
                 list1 = list(filter(lambda x: x.id == selection, biome1))
-                # search the coast list for the selection
+                # search list2 for the selection
                 list2 = list(filter(lambda x: x.id == selection, biome2))
                 if list1 != []:
-                    # add the animal to the river that was found in the search
+                    # add the animal to the biome that was found in the search
                     list1[0].add_animal(animal)
                 elif list2 != []: 
-                    # add the animal to the river that was found in the search
+                    # add the animal to the biome that was found in the search
                     list2[0].add_animal(animal)
                     # clear screen and print success message
                 os.system("cls" if os.name == "nt" else "clear")
@@ -70,8 +70,9 @@ def create_animal_two_habitats(biome1, biome2, arb, animal):
             input("\n\nPress enter to continue...")
 
 
-def create_animal_one_habitat():
-    if arboretum.forests == []:
+def create_animal_one_habitat(biome1, arb, animal):
+    os.system("cls" if os.name == "nt" else "clear")
+    if biome1 == []:
         # clear the screen and print an error
         os.system("cls" if os.name == "nt" else "clear")
         # Printing the header
@@ -81,31 +82,31 @@ def create_animal_one_habitat():
         print("Uh Oh! There are no biomes for this animal to live in. Please go create a biome for this animal.")
         input("\n\nPress enter to continue...")
     else: 
-        gecko_habitats = list()
-        for forest in arboretum.forests:
+        animal_list = list()
+        for habitat in biome1:
             # if the biome has less than 20 animal in it, add it to the list
-            if forest.is_animals_full() == False:
-                gecko_habitats.append(forest)
-        if gecko_habitats != []:
+            if habitat.is_animals_full() == False:
+                animal_list.append(habitat)
+        if animal_list != []:
         # loop over the biomes and display them in terminal
         # Printing the header
             print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
  |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
  +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-            for index, forest in enumerate(gecko_habitats):
-                print(f'{index + 1}. Forest ({forest.current_animals} animals)')
+            for index, habitat in enumerate(animal_list):
+                print(f'{index + 1}. {habitat.name} ({habitat.current_animals} animals)')
             print("\nRelease the animal into which habitat?\n")
 
             choice2 = input("Type M to return to the main menu. > ")
 
             try:
                 # capture the selection 
-                selection = gecko_habitats[int(choice2) - 1].id
-                # filter the aboretum.forest list for the selection
-                forest = list(filter(lambda x: x.id == selection, arboretum.forests))
+                selection = animal_list[int(choice2) - 1].id
+                # filter the aboretum.biome list for the selection
+                new_animal = list(filter(lambda x: x.id == selection, biome1))
                 # append the animal to the biome list
 
-                forest[0].add_animal(animal)
+                new_animal[0].add_animal(animal)
                 # clear screen and print success message
                 os.system("cls" if os.name == "nt" else "clear")
                 # Printing the header
@@ -115,7 +116,7 @@ def create_animal_one_habitat():
                 print(F'Success! The {animal.species} has been added to the biome!')
                 input("\n\nPress enter to continue...")
             except: 
-                release_animal(arboretum)
+                create_animal_one_habitat(biome1, arb, animal)
         else: 
             # print this if there are biomes created but they are all full
             print("Uh-oh! It looks like all of the biomes are full! Please go create a new Habitat for this animal to live in.")
--- actions/release_animal.py
+++ actions/release_animal.py
@@ -2,7 +2,7 @@
 # Purpose: Holds the logic that creates a new animal and adds it to a biome
 from animals import RiverDolphin, nenegoose, Ulae, Kikakapu, Pueo, Opeapea, HHFSpider, GDDGecko
 import os
-from .create_animal import create_animal_two_habitats
+from .create_animal import create_animal_two_habitats, create_animal_one_habitat
 def release_animal(arboretum):
     animal = None
     # clears the screen
@@ -41,110 +41,14 @@ Type M to return to the main menu.
     if choice == "2":
         os.system("cls" if os.name == "nt" else "clear")
         animal = GDDGecko()
-        if arboretum.forests == []:
-            # clear the screen and print an error
-            os.system("cls" if os.name == "nt" else "clear")
-            # Printing the header
-            print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-            print("Uh Oh! There are no biomes for this animal to live in. Please go create a biome for this animal.")
-            input("\n\nPress enter to continue...")
-        else: 
-            gecko_habitats = list()
-            for forest in arboretum.forests:
-                # if the biome has less than 20 animal in it, add it to the list
-                if forest.is_animals_full() == False:
-                    gecko_habitats.append(forest)
-            if gecko_habitats != []:
-                # loop over the biomes and display them in terminal
-                # Printing the header
-                print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-                for index, forest in enumerate(gecko_habitats):
-                    print(f'{index + 1}. Forest ({forest.current_animals} animals)')
-                print("\nRelease the animal into which habitat?\n")
-
-                choice2 = input("Type M to return to the main menu. > ")
-
-                try:
-                    # capture the selection 
-                    selection = gecko_habitats[int(choice2) - 1].id
-                    # filter the aboretum.forest list for the selection
-                    forest = list(filter(lambda x: x.id == selection, arboretum.forests))
-                    # append the animal to the biome list
-
-                    forest[0].add_animal(animal)
-                    # clear screen and print success message
-                    os.system("cls" if os.name == "nt" else "clear")
-                    # Printing the header
-                    print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-                    print(F'Success! The {animal.species} has been added to the biome!')
-                    input("\n\nPress enter to continue...")
-                except: 
-                    release_animal(arboretum)
-            else: 
-                # print this if there are biomes created but they are all full
-                print("Uh-oh! It looks like all of the biomes are full! Please go create a new Habitat for this animal to live in.")
-                input("\n\nPress enter to continue...")
-
-
+        create_animal_one_habitat(arboretum.forests, arboretum, animal)
 
 
     if choice == "3":
         os.system("cls" if os.name == "nt" else "clear")
         animal = nenegoose()
-        if arboretum.grasslands == []:
-            # clear the screen and print an error
-            os.system("cls" if os.name == "nt" else "clear")
-            # Printing the header
-            print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-            print("Uh Oh! There are no biomes for this animal to live in. Please go create a biome for this animal.")
-            input("\n\nPress enter to continue...")
-        else: 
-            nenegoose_habitats = list()
-            for land in arboretum.grasslands:
-                if land.is_animals_full() == False:
-                    nenegoose_habitats.append(land)
-            if nenegoose_habitats != []:
-                # loop over the biomes and display them in terminal
-                # Printing the header
-                print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-                for index, grassland in enumerate(nenegoose_habitats):
-                    print(f'{index + 1}. Grassland ({grassland.current_animals} animals)')
-                print("\nRelease the animal into which habitat?\n")
-
-                choice3 = input("Type M to return to the main menu. > ")
-
-                try: 
-                    selection = nenegoose_habitats[int(choice3) - 1].id
-                    # append the animal to the biome list
-                    land = list(filter(lambda x: x.id == selection, arboretum.grasslands))
-
-                    # append the animal to the biome list
-                    land[0].add_animal(animal)
-                    # clear screen and print success message
-                    os.system("cls" if os.name == "nt" else "clear")
-                    # Printing the header
-                    print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-                    print(F'Success! The {animal.species} has been added to the habitat!')
-                    input("\n\nPress enter to continue...")
-                except: 
-                    release_animal(arboretum)
-            else: 
-                # print this if there are biomes created but they are all full
-                print("Uh-oh! It looks like all of the biomes are full! Please go create a new Habitat for this animal to live in.")
-                input("\n\nPress enter to continue...")
-
+        create_animal_one_habitat(arboretum.grasslands, arboretum, animal)
+        
 
 
 
@@ -214,48 +118,4 @@ Type M to return to the main menu.
     if choice == "8":
         os.system("cls" if os.name == "nt" else "clear")
         animal = HHFSpider()
-        if arboretum.swamps == []:
-            os.system("cls" if os.name == "nt" else "clear")
-            # Printing the header
-            print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-            print("Uh Oh! There are no biomes for this animal to live in. Please go create a biome for this animal.")
-            input("\n\nPress enter to continue...")
-        else: 
-            spider_habitats = list()
-            for swamp in arboretum.swamps:
-                if swamp.is_animals_full() == False:
-                    spider_habitats.append(swamp)
-            if spider_habitats != []:
-                # Printing the header
-                print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-                for index, swamp in enumerate(spider_habitats):
-                    print(f'{index + 1}. Swamp ({swamp.current_animals} animals)')
-                print("\nRelease the animal into which habitat?\n")
-
-                choice8 = input("Type M to return to the main menu. > ")
-                try:
-                    selection = spider_habitats[int(choice8) - 1].id
-                    # append the animal to the biome list
-                    swamp = list(filter(lambda x: x.id == selection, arboretum.swamps))
-
-                    swamp[0].add_animal(animal)
-                    os.system("cls" if os.name == "nt" else "clear")
-                    # Printing the header
-                    print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-                    print(F'Success! The {animal.species} has been added to the biome!')
-                    input("\n\nPress enter to continue...")
-                except: 
-                    release_animal(arboretum)
-            else: 
-                # print this if there are biomes created but they are all full
-                print("Uh-oh! It looks like all of the biomes are full! Please go create a new Habitat for this animal to live in.")
-                input("\n\nPress enter to continue...")
-
-
-
+        create_animal_one_habitat(arboretum.swamps, arboretum, animal)
