6cd94ec2566ffcc0556075a41fc8213a834f2ed2
Lauren Riddle <lari7132@gmail.com>
2020-01-28 10:14:13 -0600

    added comments

--- actions/create_animal.py
+++ actions/create_animal.py
@@ -18,14 +18,14 @@ def create_animal_two_habitats(biome1, biome2, arb, animal):
 
     else: 
 
-        # combine the biome lists
+        # combine the two biome lists
         new_list = []
         for habitat1 in biome1:
-            # add the habitat to the list if it has less than 12 animals in it
+            # add the habitat to the list if it is not full
             if habitat1.is_animals_full() == False:
                 new_list.append(habitat1)
         for habitat2 in biome2:
-            # add the coastline to the list if it has less than 15 animals in it
+            # add the habitat to the list if it is not full
             if habitat2.is_animals_full() == False:
                 new_list.append(habitat2)
 
@@ -34,20 +34,22 @@ def create_animal_two_habitats(biome1, biome2, arb, animal):
             print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
  |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
  +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-            # loop over list and display in terminal
+            # loop over combined list and display in terminal
             for index, place in enumerate(new_list):
                 print(f'{index + 1}. {place.name} ({place.current_animals} animals)')
             print("\nRelease the animal into which habitat?\n")
             choice1 = input('''Type M to return to the main menu.
                  
 > ''')
+            # if the user types M, take them to the main menu
             if choice1.lower() == "m":
                 return
             try:
+            # otherwise, add the animal to the habitat
                 selection = new_list[int(choice1) - 1].id
-                # search the list1 for the selection
+                # search list1 (biome1) for the selection
                 list1 = list(filter(lambda x: x.id == selection, biome1))
-                # search list2 for the selection
+                # search list2 (biome2) for the selection
                 list2 = list(filter(lambda x: x.id == selection, biome2))
                 if list1 != []:
                     # add the animal to the biome that was found in the search
@@ -86,15 +88,15 @@ def create_animal_one_habitat(biome1, arb, animal):
     else: 
         animal_list = list()
         for habitat in biome1:
-            # if the biome has less than 20 animal in it, add it to the list
+            # if the biome is not full, add it to the list
             if habitat.is_animals_full() == False:
                 animal_list.append(habitat)
         if animal_list != []:
-        # loop over the biomes and display them in terminal
         # Printing the header
             print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
  |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
  +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
+        # loop over the biomes and display them in terminal
             for index, habitat in enumerate(animal_list):
                 print(f'{index + 1}. {habitat.name} ({habitat.current_animals} animals)')
             print("\nRelease the animal into which habitat?\n")
