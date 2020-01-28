e8d3be78055cac8cf6d1e2837367ee44bfab4c5b
Lauren Riddle <lari7132@gmail.com>
2020-01-28 10:05:16 -0600

    added module to option 6

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -35,8 +35,6 @@ Type M to return to the main menu.
         animal = RiverDolphin()
         create_animal_two_habitats(arboretum.rivers, arboretum.coastlines, arboretum, animal)
 
-            
-
 
     if choice == "2":
         os.system("cls" if os.name == "nt" else "clear")
@@ -49,8 +47,6 @@ Type M to return to the main menu.
         animal = nenegoose()
         create_animal_one_habitat(arboretum.grasslands, arboretum, animal)
         
-
-
 
     if choice == "4":
         os.system("cls" if os.name == "nt" else "clear")
@@ -67,49 +63,8 @@ Type M to return to the main menu.
     if choice == "6":
         os.system("cls" if os.name == "nt" else "clear")
         animal = Ulae()
-        if arboretum.coastlines == []:
-            os.system("cls" if os.name == "nt" else "clear")
-            # Printing the header
-            print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-            print("Uh Oh! There are no biomes for this animal to live in. Please go create a biome for this animal.")
-            input("\n\nPress enter to continue...")
-        else: 
-            ulae_habitats = list()
-            for coast in arboretum.coastlines:
-                if coast.is_animals_full() == False:
-                    ulae_habitats.append(coast)
-            if ulae_habitats != []:
-                # Printing the header
-                print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-                for index, coastline in enumerate(ulae_habitats):
-                    print(f'{index + 1}. Coastline ({coastline.current_animals} animals)')
-                print("\nRelease the animal into which habitat?\n")
-
-                choice6 = input("Type M to return to the main menu. > ")
-                try: 
-                    selection = ulae_habitats[int(choice6) - 1].id
-                    # append the animal to the biome list
-                    coast = list(filter(lambda x: x.id == selection, arboretum.coastlines))
-
-                    coast[0].add_animal(animal)
-                    os.system("cls" if os.name == "nt" else "clear")
-                    # Printing the header
-                    print( ''' +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+
- |  K  e  a  h  u  a    A  r  b  o  r  e  t  u  m  |
- +-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-++-+\n''')
-                    print(F'Success! The {animal.species} has been added to the biome!')
-                    input("\n\nPress enter to continue...")
-                except: 
-                    release_animal(arboretum)
-            else: 
-                # print this if there are biomes created but they are all full
-                print("Uh-oh! It looks like all of the biomes are full! Please go create a new Habitat for this animal to live in.")
-                input("\n\nPress enter to continue...")
-
+        create_animal_one_habitat(arboretum.coastlines, arboretum, animal)
+       
     if choice == "7":
         os.system("cls" if os.name == "nt" else "clear")
         animal = Opeapea()
